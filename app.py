7af14517fadbd9678c69b37a69f5a48bca94f1f5
ritalamykin <88649100+ritalamykin@users.noreply.github.com>
2023-09-08 18:42:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ file.close()
 
 app = Flask(__name__)
 
-#port = int(os.environ.get("PORT", 5000))
+port = int(os.environ.get("PORT", 5000))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -40,4 +40,4 @@ def index():
 
 if __name__ == "__main__":
    # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=port)
